6b98166ac78de6f8d613a4699d881998cc8b2e1a
tobitherobot <baumann.tobias@outlook.com>
2022-10-11 20:25:28 +0200

    added descr

--- spotify-endsong.py
+++ spotify-endsong.py
@@ -1,12 +1,14 @@
 import os
 import json
 
-file_count = 14
-limit_year = "2018"
+file_count = 14             # amount of endsong files
+date_limit = "2018-01-01"   # date limit; streams equal or later will be ignored
+
+# platforms related to my streams
+my_platforms = ["Windows 10 (10.0.10586; x64)", "Windows 10 (10.0.14393; x64)", "Windows 10 (10.0.15063; x64)", "Windows 7 (6.1.7601; x64; SP1; S)", "Windows 8 (6.2.9200; x64)", "Windows 8.1 (6.3.9600; x64)", "Android OS 4.4.2 API 19 (HTC, HTC One mini)", "Partner samsung_2014_tv_v7a8 Samsung 2012 TV", "Android [arm 0]", "iOS 7.0 (iPad3,6)", "iOS 7.1.2 (iPhone3,1)", "WebPlayer (websocket RFC6455)"]
 
 abs_path = os.path.dirname(__file__)
 rel_path = "prod\endsong_{}.json"
-my_platforms = ["Windows 10 (10.0.10586; x64)", "Windows 10 (10.0.14393; x64)", "Windows 10 (10.0.15063; x64)", "Windows 7 (6.1.7601; x64; SP1; S)", "Windows 8 (6.2.9200; x64)", "Windows 8.1 (6.3.9600; x64)", "Android OS 4.4.2 API 19 (HTC, HTC One mini)", "Partner samsung_2014_tv_v7a8 Samsung 2012 TV", "Android [arm 0]", "iOS 7.0 (iPad3,6)", "iOS 7.1.2 (iPhone3,1)", "WebPlayer (websocket RFC6455)"]
 streams = []
 count = 0
 
@@ -14,13 +16,12 @@ for i in range(file_count):
 
     path = os.path.join(abs_path, rel_path.format(i))
     f = open(path, "r", encoding="utf8")
-
     s = f.read().replace("'", "\'")
     d = json.loads(s)
     d.sort(key=lambda x: x['ts'])
 
     for stream in d:
-        if stream['ts'][0:4] < limit_year:
+        if stream['ts'][0:10] < date_limit:
             if stream['platform'] in my_platforms and stream['master_metadata_track_name'] != None:
                 streams.append(stream)
                 count += 1
